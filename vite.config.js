168f5f79aa3e250dd43f6d77877225ca10f59def
Bulava Eduard <bulavaeduard@gmail.com>
2025-12-30 22:06:09 +0200

    Виправлено base URL для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/minacraft/', // Замініть на назву вашого репозиторію
+  base: '/minecraft/', // Назва репозиторію без "a"
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
